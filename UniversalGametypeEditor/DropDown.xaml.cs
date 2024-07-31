afb4930b74f2969a177d82bd6bbbb8a5d850e3dd
Sopitive <30811278+Sopitive@users.noreply.github.com>
2024-07-30 18:04:03 -0700

    CGB Playlist Editor UI Changes & Tutorial Updates (#10)
    * Work On Script Decompiler
    
    * Decompiler More Action Support
    
    * Work on Decompiler
    
    * Fix Copy Sometimes Breaking
    
    * Playlist creator, Bug Fixes
    
    * HotFix VariantType Build Crash
    
    * Fix VariantTypeEnum
    
    * Hash tweaks
    
    * Updated Tutorial, Playlist Editor UI Updates

--- UniversalGametypeEditor/DropDown.xaml.cs
+++ UniversalGametypeEditor/DropDown.xaml.cs
@@ -23,6 +23,22 @@ namespace UniversalGametypeEditor
         public DropDown()
         {
             InitializeComponent();
+            Scroller.Loaded += Scroller_Loaded;
+        }
+
+        private void Scroller_Loaded(object sender, RoutedEventArgs e)
+        {
+            Scroller.ScrollChanged += Scroller_ScrollChanged;
+        }
+
+        private void Scroller_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // Show or hide the top scroll indicator
+            TopIndicator.Visibility = Scroller.VerticalOffset > 0 ? Visibility.Visible : Visibility.Collapsed;
+            double height = Scroller.ScrollableHeight;
+            height -= 10;
+            // Show or hide the bottom scroll indicator
+            BottomIndicator.Visibility = Scroller.VerticalOffset < height ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private void Expander_Expanded(object sender, RoutedEventArgs e)
